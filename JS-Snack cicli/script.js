17d6ba2c80f7370d576b5d635e4b471e1f6c0af6
Daniele <pucciodaniele95@gmail.com>
2022-05-12 11:05:48 +0200

    finish ex

--- JS-Snack cicli/script.js
+++ JS-Snack cicli/script.js
@@ -1,7 +1,14 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
-const myArray = [];
+const userArray = [];
+let sumUserArray = 0;
 
-// while (myArray.length < 50) {
+do {
+    let userNum = parseInt(prompt("Inserisci un numero"));
+    userArray.push(userNum);
 
-// }
+    sumUserArray += userNum;
+
+} while (sumUserArray < 50);
+
+console.log(userArray);
